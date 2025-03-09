c49a7b88af30ee469416547491d054d84660d9bc
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2025-03-09 17:51:39 +0100

    Flip mit Content erstellt

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const rotate = document.querySelector('.fa-rotate');
     const formContainer = document.querySelector('.formContainer');
     const welcome = document.querySelector('.bigHeadliner');
+    const me = document.querySelector('.textAboutMe');
+
+    me.style.visibility = 'hidden';
     
     rotate.addEventListener('click', () => {
         if(formContainer.classList.contains('flip')) {
             formContainer.classList.remove('flip');
             formContainer.classList.add('reverseFlip');
             welcome.innerHTML = 'Schreib mir eine Mail und ich melde mich bei Dir!';
+            me.style.visibility = 'hidden';  // Zeigen beim Zurückdrehen
         } else {
             formContainer.classList.remove('reverseFlip');
             formContainer.classList.add('flip');
             welcome.innerHTML = 'Hi, ich bin Yassin! 👋';
+            me.innerHTML = `
+                <p>Ich bin 20 Jahre alt, komme aus Köln und studiere Medieninformatik!</p>
+                <ul>
+                    <li class = "listObjects">3 Semester Tutor für Mathe 1 & 2</li>
+                    <li class = "listObjects">Momentan im 5. Semester an der TH Köln</li>
+                    <li class = "listObjects">Abi im 2022 mit Schwerpunkt Mathemathik</li>
+                </ul>
+            `;
+            setTimeout(() => {
+                me.style.visibility = 'visible';  // Zeigen beim Hindrehen
+            }, 200)
         }
     });
 });
